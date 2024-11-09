d7810af358687d2891b8b7cbe68bb5e78c353592
polarfarmer <a01040798163@gmail.com>
2024-11-09 12:57:01 +0900

    SQL-2

---
+++ src/ui/DBC.java
@@ -0,0 +1,29 @@
+public class DBC {
+    private int id;
+    private int score;
+    private int name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getName() {
+        return name;
+    }
+
+    public void setName(int name) {
+        this.name = name;
+    }
+}
---
+++ src/ui/MineDAO.java
@@ -0,0 +1,14 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class MineDAO {
+    public static void addScore(int score) throws SQLException {
+        String sql = "INSERT INTO tetris_board (score) VALUES (?)";
+        try (Connection conn = DBConnection.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql))
+        {
+            pstmt.setInt(1,score);
+            pstmt.executeUpdate();
+        }
+    }
+}
