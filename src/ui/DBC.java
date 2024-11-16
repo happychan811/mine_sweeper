95218ea5795b597def4018f2af84299cd84f52ce
polarfarmer <a01040798163@gmail.com>
2024-11-16 11:16:39 +0900

    +mine,map

--- src/ui/DBC.java
+++ src/ui/DBC.java
@@ -1,7 +1,8 @@
 public class DBC {
     private int id;
     private int score;
-    private int name;
+    private int mine;
+    private int map;
 
     public int getId() {
         return id;
@@ -17,5 +18,21 @@ public class DBC {
 
     public void setScore(int score) {
         this.score = score;
+    }
+
+    public int getMine() {
+        return mine;
+    }
+
+    public void setMine(int mine) {
+        this.mine = mine;
+    }
+
+    public int getMap() {
+        return map;
+    }
+
+    public void setMap(int map) {
+        this.map = map;
     }
 }
--- src/ui/MineDAO.java
+++ src/ui/MineDAO.java
@@ -6,11 +6,13 @@ import java.sql.SQLException;
 
 public class MineDAO {
 
-    public static void addScore(int score) throws SQLException {
-        String sql = "INSERT INTO tetris_board (score) VALUES (?)";
+    public static void addScore(int score,int mine, int map) throws SQLException {
+        String sql = "INSERT INTO tetris_board (score) VALUES (?,?,?)";
         try (Connection conn = DBConnection.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql))
         {
-            pstmt.setInt(2,score);
+            pstmt.setInt(2, score);
+            pstmt.setInt(3, mine);
+            pstmt.setInt(4, map);
             pstmt.executeUpdate();
         }
     }
