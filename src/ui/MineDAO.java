d394054f4ce703108ab74975e9f79d085aa8b173
polarfarmer <a01040798163@gmail.com>
2024-11-16 11:11:05 +0900

    SQL2

--- src/ui/MineDAO.java
+++ src/ui/MineDAO.java
@@ -10,7 +10,7 @@ public class MineDAO {
         String sql = "INSERT INTO tetris_board (score) VALUES (?)";
         try (Connection conn = DBConnection.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql))
         {
-            pstmt.setInt(1,score);
+            pstmt.setInt(2,score);
             pstmt.executeUpdate();
         }
     }
