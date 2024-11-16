1313216aac25bffc18dcb77effa2fafb83fe5a43
polarfarmer <a01040798163@gmail.com>
2024-11-16 11:04:41 +0900

    DBC

--- src/ui/MineDAO.java
+++ src/ui/MineDAO.java
@@ -1,8 +1,11 @@
+package ui;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class MineDAO {
+
     public static void addScore(int score) throws SQLException {
         String sql = "INSERT INTO tetris_board (score) VALUES (?)";
         try (Connection conn = DBConnection.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql))
