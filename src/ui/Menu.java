c8c5f56f2cc46b7d08468ec229c159f526227de3
KiwiTree3257 <kiwitree3257@gmail.com>
2024-11-30 12:20:00 +0900

    랭킹 ui

--- src/ui/Menu.java
+++ src/ui/Menu.java
@@ -4,9 +4,13 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 
-//SQL SCHEMAS = north_korea_mine_sweeper / Table = mine_sweeper / Column 1 = id[int] (PK,NN,UQ,AI) / Column 2 = score[int] (NN)
-
 public class Menu extends JFrame {
+    public CardLayout cardLayout;
+    public JPanel container;  // Container를 JPanel로 변경
+
+    private JPanel menuPanel;
+    private Ranking ranking;
+
     private JLabel title;
 
     private JPanel panelCenter;
@@ -24,12 +28,23 @@ public class Menu extends JFrame {
         setTitle("지뢰찾기");
         setSize(500, 500);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLayout(new BorderLayout());
+
+        cardLayout = new CardLayout(0, 0);
+        setLayout(cardLayout);
+
+        menuPanel = new JPanel();
+        menuPanel.setLayout(new BorderLayout());
+
+        ranking = new Ranking(this);
+
+        container = new JPanel(cardLayout);
+        container.add(menuPanel, "card_1");
+        container.add(ranking, "card_2");
 
         title = new JLabel("혁명적인 지뢰찾기");
         title.setHorizontalAlignment(SwingConstants.CENTER);
         title.setFont(new Font("궁서체", Font.BOLD, 40));
-        add(title, BorderLayout.NORTH);
+        menuPanel.add(title, BorderLayout.NORTH);
 
         panelCenter = new JPanel();
         panelCenter.setLayout(new BorderLayout());
@@ -63,7 +78,6 @@ public class Menu extends JFrame {
 
                         if (grid_size * grid_size <= total_mines) {
                             JOptionPane.showMessageDialog(getContentPane(), "지뢰의 최대값: " + (grid_size * grid_size - 1));
-
                         }
                         else {
                             new MineSweeperGame(grid_size, total_mines);
@@ -81,13 +95,15 @@ public class Menu extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 if (e.getSource() == rankButton) {
-                    new Ranking();
+                    cardLayout.next(container);
                 }
             }
         });
         panelCenter.add(startButton, BorderLayout.CENTER);
         panelCenter.add(rankButton, BorderLayout.SOUTH);
-        add(panelCenter, BorderLayout.CENTER);
+        menuPanel.add(panelCenter, BorderLayout.CENTER);
+
+        add(container);
 
         setLocationRelativeTo(null);
         setVisible(true);
